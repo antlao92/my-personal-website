62b3a79f25930c976530341418b03b6afd6636b4
Anthony <anthonykimlao@gmail.com>
2023-10-14 07:17:28 -0500

    add dropdown and skeleton of site

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
 
-document.querySelector("body").appendChild(h2);
+//Dropdown menu function
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+/*const h2 = document.createElement("h2");
+h2.textContent = "Welcome to my website!";
+document.querySelector("body").appendChild(h2);*/
